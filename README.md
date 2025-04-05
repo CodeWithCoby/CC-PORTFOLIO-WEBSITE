84fe7974e3ddff0a5662a7815a0ef63be081dbe7
CodeWithCoby <codewithcoby.com>
2025-04-05 05:03:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# One-page developer portfolio starter code
+
+This is the starter code for the one-page developer portfolio website on the codecode github https://github.com/CodeWithCoby
---
+++ js/index.js
@@ -0,0 +1,41 @@
+const home = document.getElementById("header__home");
+home.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+const secondary1 = document.getElementById("secondary1");
+secondary1.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href =
+    "https://github.com/CodeWithCoby/portfolio-website/tree/main/Build-and-Deploy-Ecommerce-Website-main";
+});
+
+const secondary2 = document.getElementById("secondary2");
+secondary2.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href =
+    "https://github.com/CodeWithCoby/portfolio-website/tree/main/task-tracker";
+});
+
+const contact__link3 = document.getElementById("contact__link3");
+contact__link3.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "https://github.com/CodeWithCoby";
+});
+
+const contact__link2 = document.getElementById("contact__link2");
+contact__link2.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "https://x.com/cobycode1";
+});
+
+const sendEmail = () => {
+  const email = "oseicoll123@gmail.com";
+  const subject = "";
+  const body = "";
+  const gmailLink = `https://mail.google.com/mail/?view=cm&to=${email}&su=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(body)}`;
+
+  window.open(gmailLink, "_blank");
+};
